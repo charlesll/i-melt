60a2f6ea77aabe99d8220602a9938bbade0042bd
Charles Le Losq <lelosq@ipgp.fr>
2020-06-16 14:39:11 +0200

    large improvements in figs

--- neuravi.py
+++ neuravi.py
@@ -577,3 +577,73 @@ def training(neuralmodel,ds,criterion,optimizer,save_name,train_patience = 50,mi
         print("Running time in seconds:", time2-time1)
 
     return neuralmodel, record_train_loss, record_valid_loss
+
+###
+### Functions for ternary plots (not really needed with mpltern)
+###
+
+def polycorners(ncorners=3):
+    '''
+    Return 2D cartesian coordinates of a regular convex polygon of a specified
+    number of corners.
+    Args:
+        ncorners (int, optional) number of corners for the polygon (default 3).
+    Returns:
+        (ncorners, 2) np.ndarray of cartesian coordinates of the polygon.
+    '''
+
+    center = np.array([0.5, 0.5])
+    points = []
+
+    for i in range(ncorners):
+        angle = (float(i) / ncorners) * (np.pi * 2) + (np.pi / 2)
+        x = center[0] + np.cos(angle) * 0.5
+        y = center[1] + np.sin(angle) * 0.5
+        points.append(np.array([x, y]))
+
+    return np.array(points)
+
+def bary2cart(bary, corners):
+    '''
+    Convert barycentric coordinates to cartesian coordinates given the
+    cartesian coordinates of the corners.
+    Args:
+        bary (np.ndarray): barycentric coordinates to convert. If this matrix
+            has multiple rows, each row is interpreted as an individual
+            coordinate to convert.
+        corners (np.ndarray): cartesian coordinates of the corners.
+    Returns:
+        2-column np.ndarray of cartesian coordinates for each barycentric
+        coordinate provided.
+    '''
+
+    cart = None
+
+    if len(bary.shape) > 1 and bary.shape[1] > 1:
+        cart = np.array([np.sum(b / np.sum(b) * corners.T, axis=1) for b in bary])
+    else:
+        cart = np.sum(bary / np.sum(bary) * corners.T, axis=1)
+
+    return cart
+
+def CLR(input_array):
+    """Transform chemical composition in colors
+    
+    Inputs
+    ------
+    input_array: n*4 array
+        4 chemical inputs with sio2, al2o3, k2o and na2o in 4 columns, n samples in rows
+        
+    Returns
+    -------
+    out: n*3 array
+        RGB colors
+    """
+    XXX = input_array.copy()
+    XXX[:,2] = XXX[:,2]+XXX[:,3] # adding alkalis
+    out = np.delete(XXX,3,1) # remove 4th row
+    # min max scaling to have colors in the full RGB scale
+    out[:,0] = (out[:,0]-out[:,0].min())/(out[:,0].max()-out[:,0].min())
+    out[:,1] = (out[:,1]-out[:,1].min())/(out[:,1].max()-out[:,1].min())
+    out[:,2] = (out[:,2]-out[:,2].min())/(out[:,2].max()-out[:,2].min())
+    return out
