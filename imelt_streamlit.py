64fb803f7b62d9eb791c79abaa7cd222c9b918e6
Charles Le Losq <lelosq@ipgp.fr>
2024-10-16 14:50:40 +0200

    add requirements file for streamlit

--- imelt_streamlit.py
+++ imelt_streamlit.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from plotly.subplots import make_subplots
+#from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 # imelt
